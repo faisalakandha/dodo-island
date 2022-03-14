3b0e1c6e6afa556e5ad52ee7faa4ed79d38d57ee
faisalakandha <57830958+faisalakandha@users.noreply.github.com>
2022-03-14 20:09:15 +0600

    (feat): draft shortcode created

--- public/shortcode.php
+++ public/shortcode.php
@@ -1,8 +1,32 @@
 <?php
 
-function dotiavatar_function() {
-    return ' <h1>Hello this is a shortcode avatar</h1> <br> <img src="http://dayoftheindie.com/wp-content/uploads/avatar-simple.png" 
-   alt="doti-avatar" width="96" height="96" class="left-align" />';
+function dotiavatar_function() { 
+    ob_start();
+    ?>
+    <table border="1">
+    <tr>
+    <th>Island Name</th>
+    <th>Dodo Code</th>
+    </tr>
+    <?php
+    global $wpdb;
+    $table_name = $wpdb->prefix . "dodo_island_manager";
+    $result = $wpdb->get_results ( "SELECT * FROM {$table_name}" );
+    foreach ( $result as $print ) {
+    ?>
+    <tr>
+    <td><?php echo $print->island_name;?></td>
+    <td><?php echo $print->dodo_code;?></td>
+    </tr>
+    <?php
+    }
+    ?>
+    </table>
+<?php
+$content = ob_get_contents();
+ob_end_clean();
+return $content;
+
 }
 
 add_shortcode('dotiavatar', 'dotiavatar_function');
